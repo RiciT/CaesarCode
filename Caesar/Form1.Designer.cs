2ff84219d88624478d6fc2c05af6b366a9b5459d
Faragó Tamás <tamas.balint.farago@gmail.com>
2023-01-08 17:20:27 +0100

    Add project files.

---
+++ Caesar/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Caesar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(16, 82);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 211);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 50);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(53, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox3.Location = new System.Drawing.Point(16, 302);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(268, 204);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox4.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox4.Location = new System.Drawing.Point(373, 82);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(268, 211);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged_1);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(373, 50);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(53, 22);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged_1);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox6.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox6.Location = new System.Drawing.Point(375, 302);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(268, 204);
+            this.textBox6.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(11, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Encrypt";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(544, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Decrypt";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(79, 54);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Letter shift";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(439, 54);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Letter shift";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox1.Location = new System.Drawing.Point(522, 50);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(129, 21);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Has letter shift?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "EN",
+            "HU"});
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "EN",
+            "HU"});
+            this.comboBox1.Location = new System.Drawing.Point(256, 21);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(104, 24);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.Text = "EN";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(175, 23);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 23);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Language";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(672, 523);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Caesar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Caesar/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Caesar
+{
+    public partial class Form1 : Form
+    {
+        string text;
+        int shift = 0;
+        bool randomize = true;
+        bool fromFile = false;
+        string docPath = Directory.GetCurrentDirectory() + @"\";
+        string fileName = "file.txt";
+        string[] words;
+        string[] ENwords = { "love", "word", "lorem", "have", "from", "ipsum", "any", "none",
+            "from", "you", "they", "their", "them", "with", "that", "this", "would", "make", "take",
+            "pick", "about", "know", "time", "there", "year", "when", "which", "some", "people", "into",
+            "him", "your", "could", "these", "want", "day", "because", "more", "look", "thing", "very", "lol",
+            "be", "of", "the", "and", "he", "she", "it", "we", "our", "but", "must", "hello", "hi" };
+        string[] HUwords = { "szia", "és", "vagy", "vagyok", "te", "köszi", "meg", "volt", "még", "már",
+            "kell", "mint", "az", "akkor", "sem", "lehet", "minden", "mert", "lehet", "nem", "olyan",
+            "szerint", "pedig", "ez", "így", "után", "úgy", "nagy", "fel", "majd", "két", "nagyon",
+            "aki", "most", "több", "lesz", "itt", "ami", "között", "hanem", "nincs", "más", "illetve" };
+        string[] abc = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", ",", "'",
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "/", "@", "#", "!", "-", "=", "+",
+            "*", "$", "%", "^", "&", "(", ")", "|", "{", "}", "[", "]", "ö", "ü", "ó", "ő", "ú", "é",
+            "á", "ű", "í", "<", ">", "?", "." };
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+        void Init()
+        {
+            if (comboBox1.Text == "EN")
+            {
+                words = ENwords;
+            }
+            else if (comboBox1.Text == "HU")
+            {
+                words = HUwords;
+            }
+            /*for (int i = 0; i < comboBox1.Items.Count; i++)
+            {
+                if (comboBox1.Text == comboBox1.Items[i].ToString())
+                {
+                    words = //how to assign variable with only knowing a string of the variable name
+                }
+            }*/
+            #region textbox
+            textBox3.ReadOnly = true;
+            textBox3.BorderStyle = 0;
+            textBox3.BackColor = this.BackColor;
+            textBox3.TabStop = false;
+            //
+            textBox6.ReadOnly = true;
+            textBox6.BorderStyle = 0;
+            textBox6.BackColor = this.BackColor;
+            textBox6.TabStop = false;
+
+            textBox2.Text = "0";
+            textBox5.Text = "0";
+            #endregion
+            if (fromFile == true)
+            {
+                int i = 0;
+                foreach (char c in System.IO.File.ReadAllText(docPath + fileName))
+                {
+                    if (i < abc.Length)
+                    {
+                        abc[i] = c.ToString();
+                        i++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            else if (randomize == true)
+            {
+                abc = Randomize(abc);
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, fileName)))
+                {
+                    outputFile.WriteLine(String.Join("", abc));
+                }
+            }
+            WordsFormatChange();
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "EN")
+            {
+                words = ENwords;
+            }
+            else if (comboBox1.Text == "HU")
+            {
+                words = HUwords;
+            }
+            /*for (int i = 0; i < comboBox1.Items.Count; i++)
+            {
+                if (comboBox1.Text == comboBox1.Items[i].ToString())
+                {
+                    words = //how to assign variable with only knowing a string of the variable name
+                }
+            }*/
+            WordsFormatChange();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0")
+            {
+                shift = 0;
+            }
+            text = textBox1.Text;
+            text = text.ToLower();
+            textBox3.Text = CaesarCode(text, shift);
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBox2.Text, out shift))
+            {
+                shift = Int32.Parse(textBox2.Text);
+            }
+        }
+        private void textBox4_TextChanged_1(object sender, EventArgs e)
+        {
+            if (textBox5.Text == "0")
+            {
+                shift = 0;
+            }
+            text = textBox4.Text;
+            text = text.ToLower();
+            if (checkBox1.Checked == true)
+            {
+                textBox6.Text = CaesarDecodeWithoutShiftValue(text);
+            }
+            else
+            {
+                textBox6.Text = CaesarDecode(text, shift);
+            }
+        }
+        private void textBox5_TextChanged_1(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(textBox5.Text, out shift))
+            {
+                shift = Int32.Parse(textBox5.Text);
+            }
+        }
+        string CaesarCode(string texttocode, int letshift)
+        {
+            int i = 0;
+            string[] codedText = new string[texttocode.Length];
+            foreach (char c in texttocode)
+            {
+                string current;
+                int index = Array.IndexOf(abc, c.ToString());
+                if (index + letshift > abc.Length - 1)
+                {
+                    index -= abc.Length;
+                }
+                current = abc[index + letshift];
+                codedText[i] = current;
+                i++;
+            }
+            return string.Join("", codedText);
+        }
+        string CaesarDecode(string texttodecode, int letshift)
+        {
+            int i = 0;
+            string[] decodedText = new string[texttodecode.Length];
+            foreach (char c in texttodecode)
+            {
+                string current;
+                int index = Array.IndexOf(abc, c.ToString());
+                if (index - letshift < 0)
+                {
+                    index += abc.Length;
+                }
+                current = abc[index - letshift];
+                decodedText[i] = current;
+                i++;
+            }
+            return string.Join("", decodedText);
+        }
+        string CaesarDecodeWithoutShiftValue(string text)
+        {
+            for (int i = 0; i < abc.Length; i++)
+            {
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (CaesarDecode(text, i).Contains(words[j]))
+                    {
+                        textBox5.Text = i.ToString();
+                        return CaesarDecode(text, i);
+                    }
+                }
+            }
+            return null;
+        }
+        public string[] Randomize(string[] input)
+        {
+            List<string> inputList = input.ToList();
+            string[] output = new string[input.Length];
+            Random r = new Random();
+            int i = 0;
+
+            while (inputList.Count > 0)
+            {
+                int index = r.Next(inputList.Count);
+                output[i++] = inputList[index];
+                inputList.RemoveAt(index);
+            }
+            return (output);
+        }
+        public void WordsFormatChange()
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = " " + words[i].ToLower() + " ";
+            }
+        }
+    }
+}
